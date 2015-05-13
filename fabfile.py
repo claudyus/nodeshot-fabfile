96329ebd3c805224619a64b4cd73c10da0d12783
Claudio Mignanti <c.mignanti@gmail.com>
2015-05-13 22:08:08 +0200

    wrap run()/sudo() functions to allow local() run

--- fabfile.py
+++ fabfile.py
@@ -20,8 +20,10 @@ def generate_random_password():
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(12))
 
 
-def cmd(*args, **kwargs):
+def run_wrapper(*args, **kwargs):
     """ use sudo if current user is not root """
+    if env['local']:
+        return local(*args, **kwargs)
     if env['user'] == 'root':
         return run(*args, **kwargs)
     else:
@@ -148,12 +150,12 @@ def initialize_db(use_defaults=False):
 
 def initialize_ssl(use_defaults=False):
     with quiet():
-        openssl_installed = run('which openssl').succeeded
+        openssl_installed = run_wrapper('which openssl').succeeded
 
     if not openssl_installed:
         print(green("openssl command not found, installing it..."))
         with hide('everything'):
-            cmd('apt-get install -y openssl')
+            run_wrapper('apt-get install -y openssl')
 
     print(green("****************************************"))
     print(green("Please insert SSL certificate details..."))
@@ -164,21 +166,21 @@ def initialize_ssl(use_defaults=False):
         openssl_command = '{0} -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN={1}"'.format(openssl_command, server_name)
 
     with cd(install_dir):
-        run(openssl_command)
+        run_wrapper(openssl_command)
 
 
 def _set_log_permissions():
-    cmd('chown -R {user}:www-data {path}/log'.format(user=env['user'], path=nodeshot_dir))
-    cmd('chmod -R 775 {path}/log'.format(path=nodeshot_dir))
+    run_wrapper('chown -R {user}:www-data {path}/log'.format(user=env['user'], path=nodeshot_dir))
+    run_wrapper('chmod -R 775 {path}/log'.format(path=nodeshot_dir))
 
 
 def create_install_dir():
     global install_dir
     print(green("Creating install dir..."))
     with hide('everything'):
-        run('mkdir -p %s' % install_dir)
+        run_wrapper('mkdir -p %s' % install_dir)
     with cd(install_dir), hide('everything'):
-        install_dir = run('pwd')
+        install_dir = run_wrapper('pwd')
 
 
 def get_os_version():
@@ -186,7 +188,7 @@ def get_os_version():
 
     version_file = 'Unknown'
     with quiet():
-        version_file = cmd('cat /etc/issue')
+        version_file = run_wrapper('cat /etc/issue')
 
     version_file = version_file.replace('\\n', '').replace('\l', '').strip()
 
@@ -205,7 +207,7 @@ def install_dependencies():
     initialize()
     print(green("Installing required packages. This will take a bit of time to download..."))
     with hide('everything'):
-        cmd('apt-get update -y')
+        run_wrapper('apt-get update -y')
         path = '{path}/dependencies.txt'.format(path=fabfile_dir)
         # read dependencies, put them on one line
         dependencies = ' '.join([line.replace('\n', '') for line in open(path).readlines()])
@@ -215,23 +217,23 @@ def install_dependencies():
             dependencies = dependencies.replace('postgresql-server-dev-9.1', 'postgresql-server-dev-9.3')
 
         # install
-        cmd('apt-get install -y %s' % dependencies)
+        run_wrapper('apt-get install -y %s' % dependencies)
         # install Postgis 2
         with quiet():
-            postgis_installed = run('dpkg --get-selections | grep "postgis\s"').succeeded
+            postgis_installed = run_wrapper('dpkg --get-selections | grep "postgis\s"').succeeded
         if not postgis_installed:
             with cd(install_dir):
-                cmd('wget http://download.osgeo.org/postgis/source/postgis-2.1.3.tar.gz')
-                cmd('tar xfvz postgis-2.1.3.tar.gz')
+                run_wrapper('wget http://download.osgeo.org/postgis/source/postgis-2.1.3.tar.gz')
+                run_wrapper('tar xfvz postgis-2.1.3.tar.gz')
             with cd('%s/postgis-2.1.3' % install_dir):
-                cmd('./configure')
-                cmd('make')
+                run_wrapper('./configure')
+                run_wrapper('make')
             # on debian 7 the procedure aborts if we don't do this
             with quiet():
                 if not exists('/usr/share/postgresql/9.1/contrib', use_sudo=use_sudo):
-                    cmd("mkdir -p '/usr/share/postgresql/9.1/contrib/postgis-2.1'")
+                    run_wrapper("mkdir -p '/usr/share/postgresql/9.1/contrib/postgis-2.1'")
             with cd('%s/postgis-2.1.3' % install_dir):
-                cmd('checkinstall -y')
+                run_wrapper('checkinstall -y')
 
 
 def create_db():
@@ -242,46 +244,46 @@ def create_db():
         db_sql = db_sql.replace('<user>', db_user)
         db_sql = db_sql.replace('<password>', db_pass)
         append(filename='/tmp/db.sql', text=db_sql, use_sudo=use_sudo)
-        cmd('chmod 777 /tmp/db.sql')
-        cmd('su - postgres -c "psql -f /tmp/db.sql"')
-        cmd('rm /tmp/db.sql')
+        run_wrapper('chmod 777 /tmp/db.sql')
+        run_wrapper('su - postgres -c "psql -f /tmp/db.sql"')
+        run_wrapper('rm /tmp/db.sql')
 
 
 def create_python_virtualenv():
     initialize()
     print(green("Creating virtual env..."))
     with hide('everything'):
-        cmd('pip install virtualenvwrapper')
-        cmd("echo 'export WORKON_HOME={workon_home}' >> /usr/local/bin/virtualenvwrapper.sh".format(workon_home=workon_home))
-        cmd("echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc")
-        cmd("echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bash_profile")
-        cmd('mkdir -p {0}'.format(workon_home))
-        cmd("chown -R {user}:{user} {workon_home}".format(user=env['user'], workon_home=workon_home))
-        cmd('chmod -R 775 {workon_home}'.format(workon_home=workon_home))
-        run('mkvirtualenv nodeshot')
+        run_wrapper('pip install virtualenvwrapper')
+        run_wrapper("echo 'export WORKON_HOME={workon_home}' >> /usr/local/bin/virtualenvwrapper.sh".format(workon_home=workon_home))
+        run_wrapper("echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc")
+        run_wrapper("echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bash_profile")
+        run_wrapper('mkdir -p {0}'.format(workon_home))
+        run_wrapper("chown -R {user}:{user} {workon_home}".format(user=env['user'], workon_home=workon_home))
+        run_wrapper('chmod -R 775 {workon_home}'.format(workon_home=workon_home))
+        run_wrapper('mkvirtualenv nodeshot')
 
 
 def install_python_requirements():
     initialize()
     print(green("Installing requirements. This will take a while, sit back and relax..."))
     with hide('everything'):
-        run('workon nodeshot && pip install -U pip wheel setuptools')
-        run('workon nodeshot && pip install -U https://github.com/ninuxorg/nodeshot/tarball/master')
+        run_wrapper('workon nodeshot && pip install -U pip wheel setuptools')
+        run_wrapper('workon nodeshot && pip install -U https://github.com/ninuxorg/nodeshot/tarball/master')
 
 
 def create_project():
     initialize()
     print(green("Creating project..."))
     with hide('everything'):
-        cmd('mkdir -p %s' % nodeshot_dir)
+        run_wrapper('mkdir -p %s' % nodeshot_dir)
     with cd(root_dir), hide('everything'):
-        cmd('workon nodeshot && nodeshot startproject %s nodeshot' % project_name)
+        run_wrapper('workon nodeshot && nodeshot startproject %s nodeshot' % project_name)
     print(green("Setting permissions..."))
     with cd(nodeshot_dir), hide('everything'):
-        cmd('chown -R %s:www-data .' % env['user'])
-        cmd('adduser www-data %s' % env['user'])
-        cmd('chmod 775 . log {name} {name}/media'.format(name=project_name))
-        cmd('chmod 750 manage.py ./%s/*.py' % project_name)
+        run_wrapper('chown -R %s:www-data .' % env['user'])
+        run_wrapper('adduser www-data %s' % env['user'])
+        run_wrapper('chmod 775 . log {name} {name}/media'.format(name=project_name))
+        run_wrapper('chmod 750 manage.py ./%s/*.py' % project_name)
 
 
 def edit_settings():
@@ -290,21 +292,21 @@ def edit_settings():
     initialize_server()
     print(green("Configuring nodeshot..."))
     with cd('%s/%s' % (nodeshot_dir, project_name)), hide('everything'):
-        cmd('sed -i \'s#<user>#%s#g\' settings.py' % db_user)
-        cmd('sed -i \'s#<password>#%s#g\' settings.py' % db_pass)
-        cmd('sed -i \'s#<domain>#%s#g\' settings.py' % server_name)
-        cmd('sed -i \'s#DEBUG = True#DEBUG = False#g\' settings.py')
+        run_wrapper('sed -i \'s#<user>#%s#g\' settings.py' % db_user)
+        run_wrapper('sed -i \'s#<password>#%s#g\' settings.py' % db_pass)
+        run_wrapper('sed -i \'s#<domain>#%s#g\' settings.py' % server_name)
+        run_wrapper('sed -i \'s#DEBUG = True#DEBUG = False#g\' settings.py')
 
 
 def install_redis():
     initialize()
     print(green("Installing redis..."))
     with hide('everything'):
-        cmd('apt-get -y --force-yes install redis-server')
-        run('workon nodeshot && pip install -U celery[redis]')
+        run_wrapper('apt-get -y --force-yes install redis-server')
+        run_wrapper('workon nodeshot && pip install -U celery[redis]')
         with warn_only():
-            cmd('echo 1 > /proc/sys/vm/overcommit_memory')
-        cmd('service redis-server start')
+            run_wrapper('echo 1 > /proc/sys/vm/overcommit_memory')
+        run_wrapper('service redis-server start')
         sleep(5)
 
 
@@ -314,10 +316,10 @@ def sync_data(update=None):
     sync_command = './manage.py syncdb --no-initial-data --noinput && ./manage.py migrate --no-initial-data && ./manage.py collectstatic --noinput'
     with cd(nodeshot_dir), hide('everything'):
         _set_log_permissions()
-        run('workon nodeshot && %s' % sync_command)
+        run_wrapper('workon nodeshot && %s' % sync_command)
         # only on install
         if not update:
-            run('workon nodeshot && ./manage.py loaddata initial_data')
+            run_wrapper('workon nodeshot && ./manage.py loaddata initial_data')
 
 
 def create_admin():
@@ -326,7 +328,7 @@ def create_admin():
     create_admin_oneliner = """echo "from nodeshot.community.profiles.models import Profile;\
                             Profile.objects.create_superuser('admin', '', 'admin')" | ./manage.py shell"""
     with cd(nodeshot_dir), hide('everything'):
-        run('workon nodeshot && %s' % create_admin_oneliner)
+        run_wrapper('workon nodeshot && %s' % create_admin_oneliner)
 
 
 def configure_nginx():
@@ -335,12 +337,12 @@ def configure_nginx():
     print(green("Configuring nginx..."))
     nginx_ssl_dir = '/etc/nginx/ssl'
     with hide('everything'):
-        cmd('mkdir -p %s' % nginx_ssl_dir)
+        run_wrapper('mkdir -p %s' % nginx_ssl_dir)
     with cd(nginx_ssl_dir), hide('everything'):
-        cmd('cp ~/nodeshot_install/server.crt .')
-        cmd('cp ~/nodeshot_install/server.key .')
-        cmd('cp /etc/nginx/uwsgi_params /etc/nginx/sites-available/')
-        cmd('mkdir -p %s/public_html' % nodeshot_dir)
+        run_wrapper('cp ~/nodeshot_install/server.crt .')
+        run_wrapper('cp ~/nodeshot_install/server.key .')
+        run_wrapper('cp /etc/nginx/uwsgi_params /etc/nginx/sites-available/')
+        run_wrapper('mkdir -p %s/public_html' % nodeshot_dir)
 
     with hide('everything'):
         nginx_conf = open('%s/nginx.conf' % fabfile_dir).read()
@@ -352,15 +354,15 @@ def configure_nginx():
                use_sudo=use_sudo)
 
     with cd('/etc/nginx/sites-available'), hide('everything'):
-        cmd('ln -s /etc/nginx/sites-available/{0} /etc/nginx/sites-enabled/{0}'.format(server_name))
-        cmd('service nginx configtest')
+        run_wrapper('ln -s /etc/nginx/sites-available/{0} /etc/nginx/sites-enabled/{0}'.format(server_name))
+        run_wrapper('service nginx configtest')
 
 
 def install_uwsgi():
     initialize()
     print(green("Installing uwsgi..."))
     with hide('everything'):
-        cmd('pip install uwsgi')
+        run_wrapper('pip install uwsgi')
         uwsgi_ini = open('%s/uwsgi.ini' % fabfile_dir).read()
         uwsgi_ini = uwsgi_ini.replace('<nodeshot_dir>', nodeshot_dir)
         uwsgi_ini = uwsgi_ini.replace('<project_name>', project_name)
@@ -391,7 +393,7 @@ def configure_supervisor():
         append(filename='/etc/supervisor/conf.d/celery-beat.conf', text=celerybeat_conf, use_sudo=use_sudo)
 
         _set_log_permissions()
-        cmd('supervisorctl update')
+        run_wrapper('supervisorctl update')
 
 
 def install_postfix():
@@ -399,7 +401,7 @@ def install_postfix():
     initialize_server()
     print(green("Installing & configuring postfix..."))
     with hide('everything'):
-        cmd('export DEBIAN_FRONTEND=noninteractive && apt-get -y install postfix')
+        run_wrapper('export DEBIAN_FRONTEND=noninteractive && apt-get -y install postfix')
         postfix_conf = open('%s/postfix.cf' % fabfile_dir).read()
         postfix_conf = postfix_conf.replace('<server_name>', server_name)
         append(filename='/etc/postfix/main.cf', text=postfix_conf, use_sudo=use_sudo)
@@ -409,17 +411,17 @@ def restart_services():
     initialize()
     print(green("Restarting nginx..."))
     with hide('everything'):
-        cmd('service nginx restart')
+        run_wrapper('service nginx restart')
     print(green("Restarting processes..."))
     with hide('everything'):
         _set_log_permissions()
-        cmd('supervisorctl restart all')
+        run_wrapper('supervisorctl restart all')
 
 
 def remove_install_dir():
     print(green("Removing install dir..."))
     with hide('everything'):
-        cmd('rm -rf ~/nodeshot_install')
+        run_wrapper('rm -rf ~/nodeshot_install')
 
 
 def check_supervisor_processes():
@@ -432,7 +434,7 @@ def check_supervisor_processes():
                 'celery-beat': False
             }
             for process in processes.keys():
-                output = cmd('supervisorctl status {0}'.format(process))
+                output = run_wrapper('supervisorctl status {0}'.format(process))
                 if 'RUNNING' in output:
                     processes[process] = True
                 elif 'STARTING' not in output:
